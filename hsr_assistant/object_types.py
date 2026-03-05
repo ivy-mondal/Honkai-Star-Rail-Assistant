01342922d6ee1f1bcdd2e6ede33a7b612e22f943
ivy-mondal <ivy94635@fmail.com>
2026-03-05 18:14:15 +0530

    purring my way through the characters 🐾💝

--- hsr_assistant/object_types.py
+++ hsr_assistant/object_types.py
@@ -51,4 +51,77 @@ class BaseCharacterStats: #For a single maxed character without relics
                  crit_rate: float, crit_damage: float,
                  break_effect: float, healing_bonus: float,
                  energy_regen_rate: float, effect_hit_rate: float,
-                 effect_res: float, elation: float, damage_bonus: float):
+                 effect_res: float, elation: float, elemental_damage_bonus: float = 0.0,
+                 hp_percent: float = 0.0, atk_percent: float = 0.0, def_percent: float = 0.0):
+        self.name = name
+        self.path = path
+        self.element = element
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.crit_rate = crit_rate
+        self.crit_damage = crit_damage
+        self.break_effect = break_effect
+        self.healing_bonus = healing_bonus
+        self.energy_regen_rate = energy_regen_rate
+        self.effect_hit_rate = effect_hit_rate
+        self.effect_res = effect_res
+        self.elation = elation
+        self.elemental_damage_bonus = elemental_damage_bonus
+        self.hp_percent = hp_percent
+        self.atk_percent = atk_percent
+        self.def_percent = def_percent
+
+
+    def get_elemental_damage_bonus_for_element(self, query_element: Element) -> float:
+        if self.element == query_element:
+            return self.elemental_damage_bonus
+        return 0.0
+
+    def __str__(self):
+        return (f"Name: {self.name.value}\n"
+                f"Path: {self.path.value}\n"
+                f"Element: {self.element.value}\n"
+                f"Health: {self.health}\n"
+                f"Attack: {self.attack}\n"
+                f"Defense: {self.defense}\n"
+                f"Speed: {self.speed}\n"
+                f"Crit Rate: {self.crit_rate}%\n"
+                f"Crit Damage: {self.crit_damage}%\n"
+                f"Break Effect: {self.break_effect}%\n"
+                f"Healing Bonus: {self.healing_bonus}%\n"
+                f"Energy Regen Rate: {self.energy_regen_rate}%\n"
+                f"Effect Hit Rate: {self.effect_hit_rate}%\n"
+                f"Effect RES: {self.effect_res}%\n"
+                f"Elation: {self.elation}\n"
+                f"{self.element.value} DMG Boost: {self.elemental_damage_bonus}%\n"
+                f"HP % from Traces: {self.hp_percent}%\n"
+                f"ATK % from Traces: {self.atk_percent}%\n"
+                f"DEF % from Traces: {self.def_percent}%\n"
+                )
+
+clara_base_stats = BaseCharacterStats(   #5
+    name= CharacterName.CLARA,
+    path= Path.DESTRUCTION,
+    element= Element.PHYSICAL,
+    health=1241.856,
+    attack=737.352,
+    defense=485.1,
+    speed=90,
+    crit_rate=5,
+    crit_damage=50,
+    break_effect=0.0,
+    healing_bonus=0.0,
+    energy_regen_rate=0.0,
+    effect_hit_rate=0.0,
+    effect_res=0.0,
+    elation=0.0,
+    elemental_damage_bonus=14.4,
+    hp_percent=10,
+    def_percent=0.0,
+    atk_percent=28
+)
+
+
+print(clara_base_stats)
